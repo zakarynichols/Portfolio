1c723c1d4390382447793824952c18d3e23c2a0a
Zak Nichols <zaknicholsdev@gmail.com>
2020-06-21 16:44:15 -0400

    Scroll reveal animations on icons.

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ navLinks.addEventListener("click", toggle);
 function toggle() {
   narrowLinks.classList.toggle("hidden");
 }
+
+// Scroll Reveal Configuration.
+ScrollReveal({ 
+  duration: 1000,
+})
+
+var myOptions = {
+  distance: '20px',
+  origin: 'left',
+  interval: 120,
+};
+
+ScrollReveal().reveal('.reveal', myOptions)
+// ScrollReveal().reveal('.tagline', { delay: 500 })
+// ScrollReveal().reveal('.punchline', { delay: 2000 })
