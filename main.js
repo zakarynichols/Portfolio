2991c17542912be35710d0c0c472780e570b8a69
Zak Nichols <zaknicholsdev@gmail.com>
2020-06-21 19:15:03 -0400

    More animations.

--- main.js
+++ main.js
@@ -13,12 +13,15 @@ ScrollReveal({
   duration: 1000,
 })
 
-var myOptions = {
+const myOptions = {
   distance: '20px',
   origin: 'left',
   interval: 120,
 };
 
+const fadeOptions = {
+  interval: 480
+}
+
 ScrollReveal().reveal('.reveal', myOptions)
-// ScrollReveal().reveal('.tagline', { delay: 500 })
-// ScrollReveal().reveal('.punchline', { delay: 2000 })
+ScrollReveal().reveal('.fade-reveal', fadeOptions)
